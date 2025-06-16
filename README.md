94d4ee0144686f097dbc3e3b28ed8b11823960fa
vasinee04 <mr.unging2546@gmail.com>
2025-06-16 11:29:54 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+# my_first_flutterapp
+นี่คือ flutter appication ปรกของฉันในรายวิชาของ ผศ.พิศาล สุขขี
+=======
+=======
+>>>>>>> eaff2fb55bdaf2bdbfd5860fa9bc19a885133475
+# fmy_lutter
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+<<<<<<< HEAD
+>>>>>>> eaff2fb (First upload)
+=======
+>>>>>>> eaff2fb55bdaf2bdbfd5860fa9bc19a885133475
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'vasinee app'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Hello VASINEE !',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: const Color.fromARGB(255, 250, 21, 21),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'คุณกดปุ่มไปแล้ว $_counter ครั้ง',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _incrementCounter,
+              child: Text('Click Me'),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+              ),
+            ),
+            SizedBox(height: 20),
+            Icon(
+              Icons.favorite,
+              color: const Color.fromARGB(255, 250, 105, 69),
+              size: 50,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
